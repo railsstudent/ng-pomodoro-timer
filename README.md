dc813de74a67a41481bbdf216acc6e74c1f0a33d
Connie Leung <cleung334@gmail.com>
2018-03-17 11:50:30 +0800

    build poromodo timer using angular 5
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ng-pomodoro-timer
+
 Scotch.io weekly coding challenge - Pomodoro Timer
+
+## Deploy to github.io
+* ng build --prod --base-href "https://railsstudent.github.io/ng-pomodoro-timer"
+* ngh --no-silent
+
+## Github.io URL
+https://railsstudent.github.io/ng-pomodoro-timer/
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit } from '@angular/core';
+
+const timerMessages = {
+  start: 'Let the countdown begin!!',
+  running: 'Greatness is within sight!!',
+  stop: 'Never quit keep going!!'
+};
+
+enum Status {
+    STOP = 'STOP',
+    PAUSE = 'PAUSE',
+    RUNNING = 'RUNNING'
+};
+
+const TOTAL_SECONDS = 25 * 60;
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent implements OnInit {
+
+  message: string = '';
+  strMinutes: string = '';
+  strSeconds: string = '';
+  totalSeconds: number = TOTAL_SECONDS;
+  timerId: number = null;
+  status = Status.STOP;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.message = timerMessages.start;
+    this.displayTime();
+  }
+
+  countdown() {
+    this.timerId = setInterval( 
+      function() {
+          if (this.totalSeconds <= 0) {
+            clearInterval(this.timerId);
+            this.setStatus(Status.STOP);
+            this.displayTime()
+          }
+          this.displayTime();
+          this.totalSeconds -= 1;
+      }.bind(this), 1000);
+  }
+
+
+  displayTime() {
+    const seconds = this.totalSeconds % 60;
+    const minutes = Math.floor((this.totalSeconds - seconds) / 60);
+
+    this.strMinutes = (minutes < 10) ? `0${minutes}` : `${minutes}`;
+    this.strSeconds = (seconds < 10) ? `0${seconds}` : `${seconds}`;;
+  }
+
+  startTimer() {
+    this.setStatus(Status.RUNNING);
+    this.countdown();
+  }
+
+  pauseTimer() {
+    clearInterval(this.timerId);
+    this.setStatus(Status.PAUSE);
+  }
+
+  stopTimer() {
+    clearInterval(this.timerId);
+    this.setStatus(Status.STOP);
+    this.displayTime()
+  }
+
+  setStatus(newStatus: Status) {
+    this.status = newStatus;
+    switch (newStatus) {
+      case Status.STOP:
+        this.message = timerMessages.start;
+        this.totalSeconds = TOTAL_SECONDS;
+        break;
+      case Status.RUNNING:
+        this.message = timerMessages.running;
+        break;
+      case Status.PAUSE:
+        this.message = timerMessages.stop;
+        break;
+      default:
+        break;
+    }
+  }
+}
